9b9bb3171cc45eeaa4e6232e5ead368378f84b3f
Paul Moreland <office@iMac-Pro.home>
2019-09-26 16:23:56 +0200

    reports edit

--- routes/reports.js
+++ routes/reports.js
@@ -3,19 +3,19 @@ var router = express.Router();
 const error = require("../models/error.js");
 const db = require("../db/database.js");
 
-router.get('/:id', async function(req, res) {
+router.get('/:id', function(req, res) {
     let sql = "SELECT * FROM report WHERE title = ?";
     let params;
     let id = req.params.id;
 
     if (req.params.id === "10") {
-        params = ["Kmom" + id];
+        params = [`Kmom${id}`];
     } else {
-        params = ["Kmom0" + id];
+        params = [`Kmom0${id}`];
     }
     db.get(sql, params, function (err, row) {
         if (err) {
-            return error.database(res, "/reports/week/" + id, err);
+            return error.database(res, `/reports/week/${id}`, err);
         }
         if (row) {
             res.json({
